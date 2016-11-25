d5098bfd51803838e17102b9c48adbed78d32eb0
bicSmera42 <belluccidamiano@gmail.com>
2016-11-25 17:27:06 +0100

    Update palindromo.c

--- palindromo.c
+++ palindromo.c
@@ -23,12 +23,15 @@ int palindromo(char* path){
         }
         else{//inizio controllo della stringa palindroma
             inizio=0;
-            fine=n;
+            fine=n-1;
             pal=1;
             
+            //printf("%c  %c",app[inizio],app[fine]);
             while (pal==1 && i<=(inizio+fine)/2) {
                 i++;
-                if (app[inizio]!=app[fine-1]){
+                inizio++;
+                fine--;
+                if (app[inizio]!=app[fine]){
                     pal=0;
                 }
                 
